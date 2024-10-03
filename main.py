87391111b5fbb0d06cd3ea312a9edc1b4eba39e1
Radu Chiriac <radu@zerodawn.tech>
2024-10-03 19:28:25 +0300

    Removed database files

--- main.py
+++ main.py
@@ -31,7 +31,7 @@ def load_user(user_id):
 class Base(DeclarativeBase):
     pass
 
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DB_URI", "sqlite:///posts.db")
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DB_URI", "sqlite:///blog.db")
 db = SQLAlchemy(model_class=Base)
 db.init_app(app)
 
